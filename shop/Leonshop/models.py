82543bf024f1c6ec301ebc916492693b2d078acc
Linemill12 <111169687+Linemill12@users.noreply.github.com>
2024-07-21 16:52:45 +0400

    Добавила отображение отзывов на сайт

--- shop/Leonshop/models.py
+++ shop/Leonshop/models.py
@@ -8,3 +8,8 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    author = models.CharField(max_length=128)
+    rating = models.IntegerField()
+    text = models.TextField()
